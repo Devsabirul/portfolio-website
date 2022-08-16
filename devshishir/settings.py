4b7b9a1d4705cbc80ce923bc1a67b447e672b7e0
Devsabirul <mdsabirulislam8089@gmail.com>
2022-08-16 14:46:15 +0600

    helo

--- devshishir/settings.py
+++ devshishir/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
@@ -135,5 +137,6 @@ CLOUDINARY_STORAGE = {
     'API_KEY': '254415131278637',
     'API_SECRET': 'xxsFxHgzn_2zRoqdzG2ki4aWQ8c'
 }
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 django_heroku.settings(locals())
